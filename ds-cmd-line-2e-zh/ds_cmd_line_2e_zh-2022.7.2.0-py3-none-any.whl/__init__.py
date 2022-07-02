ed2a02fe3c53cd9f0c31db20c17d811e086176a8
Tom Forbes <tom@tomforb.es>
2022-07-02 05:37:42 +0000

    {"name":"ds-cmd-line-2e-zh","version":"2022.7.2.0","file":"ds_cmd_line_2e_zh-2022.7.2.0-py3-none-any.whl","path":"ds-cmd-line-2e-zh/ds_cmd_line_2e_zh-2022.7.2.0-py3-none-any.whl"}

---
+++ ds-cmd-line-2e-zh/ds_cmd_line_2e_zh-2022.7.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ds-cmd-line-2e-zh
+https://github.com/apachecn/ds-cmd-line-2e-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.7.2.0"
